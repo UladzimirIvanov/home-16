2b2db8d66338cf8ee5dc71c4bb3531b27ec4c1b2
UladzimirIvanov <ivanouum@gmail.com>
2024-05-18 11:31:07 +0300

    Result Task1

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,2 +1,22 @@
-package Task1;public class Main {
+package Task1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите слова через пробел");
+        String[] userInput = scanner.nextLine().split(" ");
+        HashMap<String, Boolean> map = new HashMap<>();
+        for (String str : userInput){
+            if (map.containsKey(str)){
+                map.put(str, true);
+            } else {
+                map.put(str, false);
+            }
+        }
+        System.out.println(map);
+    }
 }
