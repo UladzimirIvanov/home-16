d4b40b2a8968b7eaa7583b7d85f7caa12bb59084
UladzimirIvanov <ivanouum@gmail.com>
2024-05-18 12:32:49 +0300

    Result Task2 men'she coda

--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -8,18 +8,11 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         HashMap<String, String> map = new HashMap<>();
         String[] userInput = scanner.nextLine().split(" ");
-        System.out.println(userInput[1].length());
-        //int lastSymbol = userInput[1].length() - 1;
         int lastSymbol;
         int firstSymbol = 0;
-        //System.out.println(userInput[1].charAt(firstSymbol) + " " + userInput[1].charAt(lastSymbol));
-        System.out.println();
-
         for (int i = 0; i < userInput.length; i++) {
             lastSymbol = userInput[i].length() - 1;
-            String fs = String.valueOf(userInput[i].charAt(firstSymbol));
-            String ls = String.valueOf(userInput[i].charAt(lastSymbol));
-            map.put(fs, ls);
+            map.put(String.valueOf(userInput[i].charAt(firstSymbol)), String.valueOf(userInput[i].charAt(lastSymbol)));
         }
         System.out.println(map);
     }
